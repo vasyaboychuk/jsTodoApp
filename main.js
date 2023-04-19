e21472efb0b8b527bf78300a156dcb452eb15c79
vasyaboychuk <boychuk98vasya@gmail.com>
2023-04-19 19:58:42 +0300

    practice js2

--- main.js
+++ main.js
@@ -7,11 +7,15 @@ button.addEventListener('click',()=>{
     li.classList.add('todo-list-item');
     const deleteBtn=document.createElement('button');
     deleteBtn.innerText='delete'
-    li.innerText=input.value;
-    list.appendChild(li);
-    li.appendChild(deleteBtn);
-    input.value=''
-    deleteBtn.addEventListener('click',()=>{
-        list.removeChild(li)
-    })
+    if (input.value){
+        li.innerText=input.value;
+        list.appendChild(li);
+        li.appendChild(deleteBtn);
+        input.value=''
+        deleteBtn.addEventListener('click',()=>{
+            list.removeChild(li)
+        })
+    }else{
+        alert('input is empty')
+    }
 })
