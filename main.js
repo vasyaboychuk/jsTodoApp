71fee1e113043bbc17d433a91c1ccd37ca07d9e4
vasyaboychuk <boychuk98vasya@gmail.com>
2023-04-19 17:24:52 +0300

    practice js

---
+++ main.js
@@ -0,0 +1,17 @@
+const button=document.querySelector('.input-container button');
+const input=document.querySelector('.input-container input');
+const list= document.querySelector('.todo-list');
+
+button.addEventListener('click',()=>{
+    const li=document.createElement('li');
+    li.classList.add('todo-list-item');
+    const deleteBtn=document.createElement('button');
+    deleteBtn.innerText='delete'
+    li.innerText=input.value;
+    list.appendChild(li);
+    li.appendChild(deleteBtn);
+    input.value=''
+    deleteBtn.addEventListener('click',()=>{
+        list.removeChild(li)
+    })
+})
